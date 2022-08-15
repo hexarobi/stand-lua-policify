5a06d80fe0c97bc85fe5a4699958f780a6b04146
Tyler Chamberlain <hexarobi@gmail.com>
2022-08-14 23:03:08 -0500

    add policify v1.2

---
+++ Policify.lua
@@ -0,0 +1,319 @@
+-- Policify 1.2
+-- by Hexarobi
+-- Enable Policify option to modify current vehicle, disable option to remove modifications
+-- Modifies horn, paint, neon, and headlights. Flashes headlights and neon between red and blue.
+
+util.require_natives(1651208000)
+
+local function show_busyspinner(text)
+    HUD.BEGIN_TEXT_COMMAND_BUSYSPINNER_ON("STRING")
+    HUD.ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME(text)
+    HUD.END_TEXT_COMMAND_BUSYSPINNER_ON(2)
+end
+
+-- From Jackz Vehicle Options script
+-- Gets the player's vehicle, attempts to request control. Returns 0 if unable to get control
+local function get_player_vehicle_in_control(pid, opts)
+    local my_ped = PLAYER.GET_PLAYER_PED_SCRIPT_INDEX(players.user()) -- Needed to turn off spectating while getting control
+    local target_ped = PLAYER.GET_PLAYER_PED_SCRIPT_INDEX(pid)
+
+    -- Calculate how far away from target
+    local pos1 = ENTITY.GET_ENTITY_COORDS(target_ped)
+    local pos2 = ENTITY.GET_ENTITY_COORDS(my_ped)
+    local dist = SYSTEM.VDIST2(pos1.x, pos1.y, 0, pos2.x, pos2.y, 0)
+
+    local was_spectating = NETWORK.NETWORK_IS_IN_SPECTATOR_MODE() -- Needed to toggle it back on if currently spectating
+    -- If they out of range (value may need tweaking), auto spectate.
+    local vehicle = PED.GET_VEHICLE_PED_IS_IN(target_ped, true)
+    if opts and opts.near_only and vehicle == 0 then
+        return 0
+    end
+    if vehicle == 0 and target_ped ~= my_ped and dist > 340000 and not was_spectating then
+        util.toast("Player is too far, auto-spectating for upto 3s.")
+        show_busyspinner("Player is too far, auto-spectating for upto 3s.")
+        NETWORK.NETWORK_SET_IN_SPECTATOR_MODE(true, target_ped)
+        -- To prevent a hard 3s loop, we keep waiting upto 3s or until vehicle is acquired
+        local loop = (opts and opts.loops ~= nil) and opts.loops or 30 -- 3000 / 100
+        while vehicle == 0 and loop > 0 do
+            util.yield(100)
+            vehicle = PED.GET_VEHICLE_PED_IS_IN(target_ped, true)
+            loop = loop - 1
+        end
+        HUD.BUSYSPINNER_OFF()
+    end
+
+    if vehicle > 0 then
+        if NETWORK.NETWORK_HAS_CONTROL_OF_ENTITY(vehicle) then
+            return vehicle
+        end
+        -- Loop until we get control
+        local netid = NETWORK.NETWORK_GET_NETWORK_ID_FROM_ENTITY(vehicle)
+        local has_control_ent = false
+        local loops = 15
+        NETWORK.SET_NETWORK_ID_CAN_MIGRATE(netid, true)
+
+        -- Attempts 15 times, with 8ms per attempt
+        while not has_control_ent do
+            has_control_ent = NETWORK.NETWORK_REQUEST_CONTROL_OF_ENTITY(vehicle)
+            loops = loops - 1
+            -- wait for control
+            util.yield(15)
+            if loops <= 0 then
+                break
+            end
+        end
+    end
+    if not was_spectating then
+        NETWORK.NETWORK_SET_IN_SPECTATOR_MODE(false, target_ped)
+    end
+    return vehicle
+end
+
+local saveData = {
+    Horn = nil,
+    Headlights_Color = nil,
+    Lights = {
+        Neon = {
+            Color = {
+                r = 0,
+                g = 0,
+                b = 0,
+            },
+            Left = false,
+            Right = false,
+            Front = false,
+            Back = false
+        }
+    },
+    Livery = {
+        Style = nil
+    }
+}
+
+local function save_headlights(vehicle)
+    saveData.Headlights_Color = VEHICLE._GET_VEHICLE_XENON_LIGHTS_COLOR(vehicle)
+end
+
+local function restore_headlights(vehicle)
+    VEHICLE._SET_VEHICLE_XENON_LIGHTS_COLOR(vehicle, saveData.Headlights_Color)
+end
+
+local function save_neon(vehicle)
+    local Color = {
+        r = memory.alloc(4),
+        g = memory.alloc(4),
+        b = memory.alloc(4),
+    }
+    VEHICLE._GET_VEHICLE_NEON_LIGHTS_COLOUR(vehicle, Color.r, Color.g, Color.b)
+    saveData.Lights.Neon = {
+        Color = {
+            r = memory.read_int(Color.r),
+            g = memory.read_int(Color.g),
+            b = memory.read_int(Color.b),
+        },
+        Left = VEHICLE._IS_VEHICLE_NEON_LIGHT_ENABLED(vehicle, 0),
+        Right = VEHICLE._IS_VEHICLE_NEON_LIGHT_ENABLED(vehicle, 1),
+        Front = VEHICLE._IS_VEHICLE_NEON_LIGHT_ENABLED(vehicle, 2),
+        Back = VEHICLE._IS_VEHICLE_NEON_LIGHT_ENABLED(vehicle, 3),
+    }
+end
+
+local function restore_neon(vehicle)
+    VEHICLE._SET_VEHICLE_NEON_LIGHT_ENABLED(vehicle, 0, saveData.Lights.Neon.Left or false)
+    VEHICLE._SET_VEHICLE_NEON_LIGHT_ENABLED(vehicle, 1, saveData.Lights.Neon.Right or false)
+    VEHICLE._SET_VEHICLE_NEON_LIGHT_ENABLED(vehicle, 2, saveData.Lights.Neon.Front or false)
+    VEHICLE._SET_VEHICLE_NEON_LIGHT_ENABLED(vehicle, 3, saveData.Lights.Neon.Back or false)
+    VEHICLE._SET_VEHICLE_NEON_LIGHTS_COLOUR(vehicle, saveData.Lights.Neon.Color.r, saveData.Lights.Neon.Color.g, saveData.Lights.Neon.Color.b)
+end
+
+local function save_paint(vehicle)
+    local Primary = {
+        Custom = VEHICLE.GET_IS_VEHICLE_PRIMARY_COLOUR_CUSTOM(vehicle),
+    }
+    local Secondary = {
+        Custom = VEHICLE.GET_IS_VEHICLE_SECONDARY_COLOUR_CUSTOM(vehicle),
+    }
+    local Color = {
+        r = memory.alloc(4),
+        g = memory.alloc(4),
+        b = memory.alloc(4),
+    }
+
+    if Primary.Custom then
+        VEHICLE.GET_VEHICLE_CUSTOM_PRIMARY_COLOUR(vehicle, Color.r, Color.g, Color.b)
+        Primary["Custom Color"] = {
+            r = memory.read_int(Color.r),
+            b = memory.read_int(Color.g),
+            g = memory.read_int(Color.b)
+        }
+    else
+        VEHICLE.GET_VEHICLE_MOD_COLOR_1(vehicle, Color.r, Color.b, Color.g)
+        Primary["Paint Type"] = memory.read_int(Color.r)
+        Primary["Color"] = memory.read_int(Color.g)
+        Primary["Pearlescent Color"] = memory.read_int(Color.b)
+    end
+    if Secondary.Custom then
+        VEHICLE.GET_VEHICLE_CUSTOM_SECONDARY_COLOUR(vehicle, Color.r, Color.g, Color.b)
+        Secondary["Custom Color"] = {
+            r = memory.read_int(Color.r),
+            b = memory.read_int(Color.g),
+            g = memory.read_int(Color.b)
+        }
+    else
+        VEHICLE.GET_VEHICLE_MOD_COLOR_2(vehicle, Color.r, Color.b)
+        Secondary["Paint Type"] = memory.read_int(Color.r)
+        Secondary["Color"] = memory.read_int(Color.g)
+    end
+    VEHICLE.GET_VEHICLE_COLOR(vehicle, Color.r, Color.g, Color.b)
+    local Vehicle = {
+        r = memory.read_int(Color.r),
+        g = memory.read_int(Color.g),
+        b = memory.read_int(Color.b),
+    }
+    VEHICLE.GET_VEHICLE_EXTRA_COLOURS(vehicle, Color.r, Color.g)
+    local ColorExtras = {
+        pearlescent = memory.read_int(Color.r),
+        wheel = memory.read_int(Color.g),
+    }
+    VEHICLE.GET_VEHICLE_COLOURS(vehicle, Color.r, Color.g)
+    Vehicle["Primary"] = memory.read_int(Color.r)
+    Vehicle["Secondary"] = memory.read_int(Color.g)
+    memory.free(Color.r)
+    memory.free(Color.g)
+    memory.free(Color.b)
+    saveData.Colors = {
+        Primary = Primary,
+        Secondary = Secondary,
+        ["Color Combo"] = VEHICLE.GET_VEHICLE_COLOUR_COMBINATION(vehicle),
+        ["Paint Fade"] = VEHICLE.GET_VEHICLE_ENVEFF_SCALE(vehicle),
+        Vehicle = Vehicle,
+        Extras = ColorExtras
+    }
+    saveData.Livery.style = VEHICLE.GET_VEHICLE_LIVERY(vehicle)
+end
+
+local function restore_paint(vehicle)
+    VEHICLE.SET_VEHICLE_MOD_KIT(vehicle, 0)
+    VEHICLE.SET_VEHICLE_COLOUR_COMBINATION(vehicle, saveData.Colors["Color Combo"] or -1)
+    if saveData.Colors.Extra then
+        VEHICLE.SET_VEHICLE_EXTRA_COLOURS(vehicle, saveData.Colors.Extras.pearlescent, saveData.Colors.Extras.wheel)
+    end
+    VEHICLE.SET_VEHICLE_CUSTOM_PRIMARY_COLOUR(vehicle, saveData.Colors.Vehicle.r, saveData.Colors.Vehicle.g, saveData.Colors.Vehicle.b)
+    VEHICLE.SET_VEHICLE_CUSTOM_SECONDARY_COLOUR(vehicle, saveData.Colors.Vehicle.r, saveData.Colors.Vehicle.g, saveData.Colors.Vehicle.b)
+    VEHICLE.SET_VEHICLE_COLOURS(vehicle, saveData.Colors.Vehicle.Primary or 0, saveData.Colors.Vehicle.Secondary or 0)
+    if saveData.Colors.Primary.Custom and saveData.Colors.Primary["Custom Color"] then
+        VEHICLE.SET_VEHICLE_CUSTOM_PRIMARY_COLOUR(vehicle, saveData.Colors.Primary["Custom Color"].r, saveData.Colors.Primary["Custom Color"].b, saveData.Colors.Primary["Custom Color"].g)
+    else
+        VEHICLE.SET_VEHICLE_MOD_COLOR_1(vehicle, saveData.Colors.Primary["Paint Type"], saveData.Colors.Primary.Color, saveData.Colors.Primary["Pearlescent Color"])
+    end
+    if saveData.Colors.Secondary.Custom and saveData.Colors.Secondary["Custom Color"] then
+        VEHICLE.SET_VEHICLE_CUSTOM_SECONDARY_COLOUR(vehicle, saveData.Colors.Secondary["Custom Color"].r,  saveData.Colors.Secondary["Custom Color"].b, saveData.Colors.Secondary["Custom Color"].g)
+    else
+        VEHICLE.SET_VEHICLE_MOD_COLOR_2(vehicle, saveData.Colors.Secondary["Paint Type"], saveData.Colors.Secondary.Color)
+    end
+    VEHICLE.SET_VEHICLE_ENVEFF_SCALE(vehicle, saveData["Colors"]["Paint Fade"] or 0)
+
+    VEHICLE.SET_VEHICLE_LIVERY(vehicle, saveData.Livery.style or -1)
+end
+
+local function save_horn(vehicle)
+    saveData.Horn = VEHICLE.GET_VEHICLE_MOD(vehicle, 14)
+end
+
+local function restore_horn(vehicle)
+    VEHICLE.SET_VEHICLE_MOD(vehicle, 14, saveData.Horn)
+end
+
+local function save_plate(vehicle)
+    saveData["License Plate"] = {
+        Text = VEHICLE.GET_VEHICLE_NUMBER_PLATE_TEXT(vehicle),
+        Type = VEHICLE.GET_VEHICLE_NUMBER_PLATE_TEXT_INDEX(vehicle)
+    }
+end
+
+local function restore_plate(vehicle)
+    ENTITY.SET_ENTITY_AS_MISSION_ENTITY(vehicle, true, true)
+    VEHICLE.SET_VEHICLE_NUMBER_PLATE_TEXT(vehicle, saveData["License Plate"].Text)
+    VEHICLE.SET_VEHICLE_NUMBER_PLATE_TEXT_INDEX(vehicle, saveData["License Plate"].Type)
+end
+
+local function policify_vehicle(vehicle)
+    -- Enable Neon
+    VEHICLE._SET_VEHICLE_NEON_LIGHT_ENABLED(vehicle, 0, true)
+    VEHICLE._SET_VEHICLE_NEON_LIGHT_ENABLED(vehicle, 1, true)
+    VEHICLE._SET_VEHICLE_NEON_LIGHT_ENABLED(vehicle, 2, true)
+    VEHICLE._SET_VEHICLE_NEON_LIGHT_ENABLED(vehicle, 3, true)
+
+    -- Police Horn
+    VEHICLE.SET_VEHICLE_MOD(vehicle, 14, 1)
+
+    -- Paint matte black
+    VEHICLE.SET_VEHICLE_CUSTOM_PRIMARY_COLOUR(vehicle, 0, 0, 0)
+    VEHICLE.SET_VEHICLE_MOD_COLOR_1(vehicle, 3, 0, 0)
+    VEHICLE.SET_VEHICLE_CUSTOM_SECONDARY_COLOUR(vehicle, 0, 0, 0)
+    VEHICLE.SET_VEHICLE_MOD_COLOR_2(vehicle, 3, 0, 0)
+
+    -- Clear livery
+    VEHICLE.SET_VEHICLE_MOD(vehicle, 48, -1)
+
+    -- Set Exempt plate
+    ENTITY.SET_ENTITY_AS_MISSION_ENTITY(vehicle, true, true)
+    VEHICLE.SET_VEHICLE_NUMBER_PLATE_TEXT_INDEX(vehicle, 4)
+    VEHICLE.SET_VEHICLE_NUMBER_PLATE_TEXT(vehicle, "FIB")
+end
+
+local policified_vehicle
+local policify_tick_counter
+local POLICIFY_CYCLE_TIME = 80
+
+local policify_ticker = function()
+    if policified_vehicle == nil then
+        util.toast("Invalid vehicle for policify ticker")
+    end
+    if policify_tick_counter % (POLICIFY_CYCLE_TIME / 2) == 0 then
+        if policify_tick_counter % POLICIFY_CYCLE_TIME == 0 then
+            VEHICLE._SET_VEHICLE_XENON_LIGHTS_COLOR(policified_vehicle, 8)
+            VEHICLE._SET_VEHICLE_NEON_LIGHTS_COLOUR(policified_vehicle, 0, 0, 255)
+        else
+            VEHICLE._SET_VEHICLE_XENON_LIGHTS_COLOR(policified_vehicle, 1)
+            VEHICLE._SET_VEHICLE_NEON_LIGHTS_COLOUR(policified_vehicle, 255, 0, 0)
+        end
+    end
+    policify_tick_counter = policify_tick_counter + 1
+end
+
+menu.toggle(menu.my_root(), "Policify", {"policify"}, "", function(on)
+    if on then
+        policified_vehicle = get_player_vehicle_in_control(players.user())
+        if policified_vehicle then
+
+            save_headlights(policified_vehicle)
+            save_neon(policified_vehicle)
+            save_horn(policified_vehicle)
+            save_paint(policified_vehicle)
+            save_plate(policified_vehicle)
+
+            policify_vehicle(policified_vehicle)
+
+            policify_tick_counter = 0
+            util.create_tick_handler(function()
+                if policify_tick_counter ~= nil then
+                    policify_ticker()
+                else
+                    return false
+                end
+            end)
+        end
+    else
+        policify_tick_counter = nil
+        restore_headlights(policified_vehicle)
+        restore_neon(policified_vehicle)
+        restore_horn(policified_vehicle)
+        restore_paint(policified_vehicle)
+        restore_plate(policified_vehicle)
+    end
+end)
+
+util.create_tick_handler(function()
+    return true
+end)
